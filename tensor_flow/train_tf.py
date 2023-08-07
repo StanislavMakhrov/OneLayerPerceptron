74db202f4e6d1e7c5cd27bd155a6e6a02bf8eed4
Stanislav Makhrov <stanislav.makhrov@gmail.com>
2023-08-07 04:26:47 +0200

    Added test after train

--- tensor_flow/train_tf.py
+++ tensor_flow/train_tf.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 from datareader import DataReader
 from datetime import datetime
 import numpy as np
+from tensor_flow.vector import Vector
 
 
 def get_max_neuron_idx(neurons):
@@ -33,28 +34,27 @@ train_dir = '../data/train'
 test_dir = '../data/test'
 
 train_generator = DataReader(train_dir, [input_height, input_width], True, input_channels, num_classes).get_generator()
-test_generator = DataReader(test_dir, [input_height, input_width], True, input_channels, num_classes).get_generator()
+test_generator = DataReader(test_dir, [input_height, input_width], False, input_channels, num_classes).get_generator()
 
 ((x_train, y_train), (x_test, y_test)) = (([], []), ([], []))
 
 for m in range(train_generator.get_data_size()):
-    image, label = train_generator.next(one_hot_encoding=False)
-    x_train.append(image)
-    y_train.append(label)
+    vector = Vector(*train_generator.next(one_hot_encoding=False))
+    x_train.append(vector.get_x())
+    y_train.append(vector.get_desired_outputs())
 
 x_train = np.array(x_train)
-y_train = np.array(y_train)
+y_train = np.array(y_train).reshape(-1)
 
 for m in range(test_generator.get_data_size()):
-    image, label = test_generator.next(one_hot_encoding=False)
-    x_test.append(image)
-    y_test.append(label)
+    vector = Vector(*test_generator.next(one_hot_encoding=False))
+    x_test.append(vector.get_x())
+    y_test.append(vector.get_desired_outputs())
 
 x_test = np.array(x_test)
-y_test = np.array(y_test)
+y_test = np.array(y_test).reshape(-1)
 
 model = keras.models.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(num_classes, activation='sigmoid'),
 ])
 
@@ -75,22 +75,20 @@ model.fit(
 )
 
 
-#     # Test the model on the entire test set
-#     print("{} Start testing".format(datetime.now()))
-#     passed = 0
-#     for _ in range(test_patterns_count):
-#         xs, ds = test_generator.next()
-#         vector = Vector(xs, ds)
-#         xs = vector.get_x()
-#         ds = vector.get_d()
+# Test the model on the entire test set
+print(f'{datetime.now()} Start testing...')
+passed = 0
+for i in range(len(x_test)):
+    predictions = model(x_test[i:i+1]).numpy()
+    print(predictions)
+
+#     prediction = sess.run(tf.nn.sigmoid(model.output), feed_dict={x: xs})
 #
-#         prediction = sess.run(tf.nn.sigmoid(model.output), feed_dict={x: xs})
+#     d_max_idx = get_max_neuron_idx(list(ds.reshape(ds[0].shape)))
+#     y_max_idx = get_max_neuron_idx(prediction[0])
+#     if y_max_idx == d_max_idx:
+#         passed += 1
+#     print("{} recognized as {}".format(d_max_idx, y_max_idx))
 #
-#         d_max_idx = get_max_neuron_idx(list(ds.reshape(ds[0].shape)))
-#         y_max_idx = get_max_neuron_idx(prediction[0])
-#         if y_max_idx == d_max_idx:
-#             passed += 1
-#         print("{} recognized as {}".format(d_max_idx, y_max_idx))
-#
-#     accuracy = passed / test_generator.get_data_size() * 100.0
-#     print("Accuracy: {:.4f}%".format(accuracy))
+# accuracy = passed / test_generator.get_data_size() * 100.0
+# print("Accuracy: {:.4f}%".format(accuracy))
